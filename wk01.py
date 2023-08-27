d123c78903b8352db6619d53df744b165d383e84
aiimix7811 <140914367+aiimix7811@users.noreply.github.com>
2023-08-28 07:59:59 +0800

    Update wk01.py

--- wk01.py
+++ wk01.py
@@ -1,9 +1,9 @@
 """
 """
-# -*- coding:utf-8 -*_
-
-cron: 1 0 4,5,11,12,13,17,18,19,23 * * * *
+# -*- coding:utf-8 -*-
+cron: 1 0 4,5,11,12,13,17,18,19,23 * * *
 new Env('悟空A本1');
+"""
 import requests,secrets,time,hashlib,string,random,json,os,sys
 import datetime
 from lib2to3.pygram import python_grammar_no_print_and_exec_statement
